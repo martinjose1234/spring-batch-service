b4f6dbb0162e117d2e9737d27c08385e57aca72d
martinjose1234 <mart.trv@gmail.com>
2023-11-09 11:44:11 +0530

    seccond commit

---
+++ src/main/java/com/batch/springbatchservice/SpringBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.batch.springbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchServiceApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchServiceApplication.class, args)));
+	}
+
+}
---
+++ src/test/java/com/batch/springbatchservice/SpringBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.springbatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
